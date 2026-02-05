36a3cfcbf2584a7f972880f213f67e79e883541d
ricardoefamilia <ricardoefamilia@gmail.com>
2026-02-05 12:34:28 -0300

    React 19 Seção 3 aula 103

---
+++ app/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Blog Next - Este é um blog feito com Next.js",
+  description: "Essa é a descrição dessa página",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body>
+        <header>
+          <h1>Blog Next</h1>
+        </header>
+        {children}
+        <footer>
+          <p>Copyright © 2024 - Blog Next</p>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return <h1>Olá, de dentro da page.tsx</h1>;
+}
+
+// app/page.tsx é o arquivo que representa a página inicial do aplicativo Next.js. é como o index.html em um site tradicional.
+// Ele é o ponto de entrada para a aplicação e é renderizado quando os usuários acessam a raiz do site ("/").
+// Ele exporta uma função chamada Home, que retorna um elemento JSX contendo um título.
+// Quando os usuários acessarem a raiz do site, eles verão a mensagem "Olá, de dentro da page.tsx".
+// app/pasta/index.tsx é o arquivo que representa a página inicial da pasta "pasta". Assim serão os demais endpoints.
