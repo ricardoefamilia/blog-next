5a44454817ab786996577d31374647ee2390351e
ricardoefamilia <ricardoefamilia@gmail.com>
2026-02-16 12:19:58 -0300

    Seção 3 - Aula 143 - pasta Out HTMLs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /* desconsiderando o svdor Next para criar páginas html */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
