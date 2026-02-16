8037fd3078b8cb7fa312c49555de816709dc2881
ricardoefamilia <ricardoefamilia@gmail.com>
2026-02-16 19:50:23 -0300

    Seção 3 - Aula 149 correção SafeMarkdown

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -5,7 +5,7 @@ type ContainerProps = {
 export function Container({ children }: ContainerProps) {
   return (
     <div className="relative text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
-      <div className="max-w-screen-lg mx-auto px-8">{children}</div>
+      <div className="max-w-5xl mx-auto px-8">{children}</div>
     </div>
   );
 }
