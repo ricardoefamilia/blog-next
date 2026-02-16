3f6a7c6406dfc0659602ab5bfbce42de21221af5
ricardoefamilia <ricardoefamilia@gmail.com>
2026-02-16 12:31:23 -0300

    Seção 3 - Aula 145 voltando a utilizar Next

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   /* desconsiderando o svdor Next para criar páginas html */
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,10 +1,7 @@
 import { SinglePost } from "@/components/SinglePost";
 import { SpinLoader } from "@/components/SpinLoader";
 // import { findPostBySlugCached } from "@/lib/post/queries";
-import {
-  findAllPublicPostsCached,
-  findPostBySlugCached,
-} from "@/lib/post/queries";
+import { findPostBySlugCached } from "@/lib/post/queries";
 import { Metadata } from "next";
 import { Suspense } from "react";
 
@@ -22,19 +19,6 @@ export async function generateMetadata({
     title: post.title,
     description: post.excerpt,
   };
-}
-
-export async function generateStaticParams() {
-  const posts = await findAllPublicPostsCached();
-  const params = posts.map((post) => {
-    return {
-      slug: post.slug,
-    };
-  });
-
-  console.log(params);
-
-  return params;
 }
 
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
