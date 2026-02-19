a1e382eaa440afda8b37cc4515ddeb30ad507360
ricardoefamilia <ricardoefamilia@gmail.com>
2026-02-19 19:00:28 -0300

    Seção 3 - Aula 170

--- src/components/PostsListAdmin/index.tsx
+++ src/components/PostsListAdmin/index.tsx
@@ -1,4 +1,8 @@
 import { findAllPostAdmin } from "@/lib/post/queries/admin";
+import clsx from "clsx";
+import Link from "next/link";
+import { DeletePostButton } from "../admin/DeletePostButton";
+import { Dialog } from "../Dialog";
 
 export default async function PostsListAdmin() {
   const posts = await findAllPostAdmin();
@@ -6,7 +10,26 @@ export default async function PostsListAdmin() {
   return (
     <div className="py-16">
       {posts.map((post) => {
-        return <p key={post.id}>{post.title}</p>;
+        return (
+          <div
+            className={clsx(
+              "py-2 px-2",
+              !post.published && "bg-slate-300",
+              "flex gap-2 items-center justify-between",
+            )}
+            key={post.id}
+          >
+            <Link href={`/admin/post/${post.id}`}>{post.title}</Link>
+
+            {!post.published && (
+              <span className="text-xs text-slate-600 italic">
+                (Não publicado)
+              </span>
+            )}
+
+            <DeletePostButton id={post.id} title={post.title} />
+          </div>
+        );
       })}
     </div>
   );
